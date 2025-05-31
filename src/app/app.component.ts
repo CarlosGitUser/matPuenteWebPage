5c36dba88fbc89d9a1ce9ff84c2152da130cdf33
JazMart12 <al336556@edu.uaa.mx>
2025-05-31 11:36:16 -0600

    cambios de header y agregue un baneer mejorado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './components/header/header.component';
+import { CTAComponent } from './components/cta/cta.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CTAComponent,
+    BannerComponent,
+    CategoriasComponent,
+    ProductosComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'matPuenteWebPage';
