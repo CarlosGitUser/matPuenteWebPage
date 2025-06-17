cfb6c39947e549d0c5d50e00f69aafd61d28f23e
JazMart12 <al336556@edu.uaa.mx>
2025-06-17 17:51:25 -0600

    solucionando errores

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
-<<<<<<< HEAD
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-=======
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes)
   ]
 });
->>>>>>> 1e3011e63fd221a6e78b6226564a61f31a12992b
+
