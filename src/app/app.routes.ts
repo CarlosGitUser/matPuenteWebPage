1e3011e63fd221a6e78b6226564a61f31a12992b
Asly Lizbeth Salinas Morales <moraleslizz913@gmail.com>
2025-06-17 17:10:38 -0600

    Cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Importación de componentes
+import { HomeComponent } from './components/home/home.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { HistoriaComponent } from './components/historia/historia.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { BlogComponent } from './components/blog/blog.component';
+// Si tienes Login y Register, agrégalos también:
+// import { LoginComponent } from './components/login/login.component';
+// import { RegisterComponent } from './components/register/register.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'acerca', component: AcercaComponent },
+  { path: 'catalogo', component: CatalogoComponent },
+  { path: 'historia', component: HistoriaComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'blog', component: BlogComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
